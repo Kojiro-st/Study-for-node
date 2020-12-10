557677de191e5ed26cdd0dbabbbdf432bcda8799
Kojiro-st <tenmadetobe5a@gmail.com>
2020-12-10 18:02:00 +0900

    eighth commit

--- fifteenth/fifteenth.js
+++ fifteenth/fifteenth.js
@@ -2,10 +2,10 @@
 
 class Calender{
   // 各月の日数を定義
-  mday(){
-    var mday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    return mday;
-  }
+  // mDay(number){
+  //   var mday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  //   return mday[number];
+  // }
 
   // year年month月day日の曜日を求める
   dayOfTheWeek(year, month, day){
@@ -13,7 +13,8 @@ class Calender{
       year--;
       month+=12;
     }
-    return (year + year/4 - year/100 + year/400 + (13*month + 8)/5 + day)/7;
+    var integer = Math.floor((year + year/4 - year/100 + year/400 + (13*month + 8)/5 + day)%7);
+    return integer+1;
   }
 
   // year年が閏年かどうかを調べる
@@ -23,26 +24,36 @@ class Calender{
 
   // year年month月の日数
   monthDays(year, month){
+    var mday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     if(month-- !=2){
-      return mDay[month];
+      return mday[month];
     }
-    return mDay[month]+(isLeap(year)?1:0);
+    if (this.isLeap(year) === true){
+      var number = 1;
+    }else{
+      var number = 0;
+    }
+    return (mday[month-1])+(number);
   }
 
   // year年month月のカレンダーの表示
   putCalender(year, month){
-    var wd = dayOfTheWeek(year, month, 1);
-    var mDays = monthDays(year, month);
+    var wd = this.dayOfTheWeek(year, month, 1);
+    var mDays = this.monthDays(year, month);
+    // mDaysは合っているっぽい。
 
-    console.log(` 日 月 火 水 木 金 `);
-    console.log(`-----------------`);
+    console.log(` 日 月 火 水 木 金 土`);
+    console.log(`----------------------`);
 
     for(var n=0; n<wd; n++){
-      process.stdout.write(' ');
+      process.stdout.write('   ');
     }
     for(var i=1; i<=mDays; i++){
-      console.log("%3d",i);
-      // %3dって何？
+      if(i<10){
+        process.stdout.write(` ${i} `);
+      }else{
+        process.stdout.write(` ${i}`);
+      }
       if(++wd%7==0){
         console.log();
       }
@@ -54,38 +65,58 @@ class Calender{
     var year =1;
     var month = 1;
 
-    if(process.argv.length<2){
+    if(process.argv.length<3){
       // 現在の日付を取り出して現在の月のカレンダーを表示する
+      var today = new Date();
+      year = today.getFullYear();
+      month = today.getMonth()+1;
+      var day = today.getDate();
+      this.putCalender(year, month)
     }else{
-      if(process.argv.length>2){
-        year = process.argv[2];
-        if(year < 0){
+      if(process.argv.length<4){
+        if(process.argv[2] < 0){
           console.log(`年の指定が不正です。`);
           return;
+        }else{
+          year = process.argv[2];
+          console.log(`${year}年のカレンダー`);
+          for(var n=1; n<=12; n++){
+            this.putCalender(year, n);
+            console.log();
+          }
         }
       }
-      if(process.argv.length>3){
-        month = process.argv[3];
-        if(month>12 || month<1){
+      if(process.argv.length<5){
+        if(process.argv[3]>12 || process.argv[3]<1){
           console.log(`月の指定が不正です。`);
           return;
         }
+        year = process.argv[2];
+        month = process.argv[3];
+        this.putCalender(year, month);
       }
     }
-    if(process.argv.length <3 || process.argv.length >4){
-      console.log(`${year}年${month}月のカレンダー`);
-      putCalender(year, month);
-    }else{
-      console.log(`${year}年のカレンダー`);
-      for(var n=1; n<=12; n++){
-        console.log(`${month}月`);
-        putCalender(year, month);
-        console.log();
-      }
-    }
+    // if(process.argv.length <3 || process.argv.length >4){
+    //   year = process.argv[2];
+    //   console.log(`${year}年${month}月のカレンダー`);
+    //   this.putCalender(year, month);
+    // }else{
+    //   console.log(`${year}年のカレンダー`);
+    //   for(var n=1; n<=12; n++){
+    //     console.log(`${month}月`);
+    //     this.putCalender(year, month);
+    //     console.log();
+    //   }
+    // }
   }
 }
 
 var test = new Calender;
 
 test.main();
+
+// 各メソッドが正常に動いているのかを確認する
+// test.dayOfTheWeek(2020,12,10);
+// test.isLeap(2020);
+// test.monthDays(2020,2);
+// test.putCalender(2020,12);
